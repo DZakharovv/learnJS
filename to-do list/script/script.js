071e8cfc013994afc9c4be302a47df2cfd9cd9a1
DZakharovv <32372191+DZakharovv@users.noreply.github.com>
2020-08-09 14:56:09 +0300

    Add files via upload

---
+++ to-do list/script/script.js
@@ -0,0 +1,85 @@
+'use sctrict';
+
+const todoControl = document.querySelector('.todo-control'),
+    headerInput = document.querySelector('.header-input'),
+    todoList = document.querySelector('.todo-list'),
+    todoItem = document.querySelector('.todo-item'),
+    // todoContainer = document.querySelector('.todo-container'),
+    todoCompleted = document.querySelector('.todo-completed');
+
+    let todoData = localStorage.getItem('todoBase') ? JSON.parse(localStorage.getItem('todoBase')) : {
+        todoList: [],
+        todoCompleted: []
+    };
+const addToStorage = function() {
+    localStorage.setItem('todoBase', JSON.stringify(todoData));
+};
+    
+// let todoData = []
+
+
+const render = function () {
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+
+
+
+    todoData.forEach(function (item, index) {
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+
+        li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+            '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+            '</div>';
+        if (item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+
+
+
+        const btnTodoComplete = li.querySelector('.todo-complete');
+
+        btnTodoComplete.addEventListener('click', function () {
+            item.completed = !item.completed;
+            render();
+            
+        });
+        addToStorage();
+        
+
+        const btnTodoRemove = li.querySelector('.todo-remove');
+
+        btnTodoRemove.addEventListener('click', function (event) {
+            // let target = event.target;
+            todoData.splice(index, 1);
+            render();
+        });
+        addToStorage();
+    });
+};
+
+todoControl.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let newTodo = {
+        value: headerInput.value.trim(),
+        completed: false,
+    }
+    if (newTodo.value !== '') {
+        todoData.push(newTodo);
+        render(); 
+        headerInput.value = '';
+    } 
+    addToStorage();
+});
+
+
+
+render();
+
+
+
